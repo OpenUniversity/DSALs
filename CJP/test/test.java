fceff6c6dd32a9d4fb1ae240174e925b560b15c5
Arik Hadas <arik.hadas@openu.ac.il>
2015-03-19 06:42:39 -0600

    last fixes for a demo

--- CJP/test/test.java
+++ CJP/test/test.java
@@ -3,7 +3,7 @@ package research;
 public class HelloWorld  {
     public static void main(String[] args) {
       exhibit say(String message) {
-        System.out.println("Hello, " + message)
+        System.out.println("Hello, " + message);
       }("World");
     }
 }
